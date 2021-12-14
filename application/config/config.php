b8d4e864182a77edd9fd5f4f81ed7842141642c0
shevaputriw <shevapw@gmail.com>
2021-12-14 09:49:52 +0700

    bas_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/simpanberkas';
+$config['base_url'] = 'https://jalasemar.mojokertokab.go.id/simpanberkas/';
 
 /*
 |--------------------------------------------------------------------------
