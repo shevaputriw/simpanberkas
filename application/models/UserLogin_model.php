74bc87f5f959222a4c49b142d513c8263d4d05dc
shevaputriw <shevapw@gmail.com>
2021-12-28 13:34:41 +0700

    crud user login

---
+++ application/models/UserLogin_model.php
@@ -0,0 +1,63 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserLogin_model extends CI_Model {
+
+    public function getAll_user() {
+        $query = $this->db->query("SELECT * FROM t9801 AS a JOIN t0021 AS b ON a.`SCIDBUID` = b.`BNIDBUID`
+        WHERE a.`SCDVS` IS NULL");
+
+        return $query->result_array();
+    }
+
+    public function getKode() {
+        $query = $this->db->query("SELECT * FROM t0020 WHERE CNCOID='16'");
+        return $query->row();
+    }
+
+    public function getOpd() {
+        $this->db->select('*');
+        $this->db->from('t0021');
+
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+
+    public function getHakAkses() {
+        $query = $this->db->query("SELECT * FROM t0009 WHERE DTPC = '00' AND DTSC = 'UG'");
+
+        return $query->result_array();
+    }
+
+    public function get_nama_opd($bndesb1) {
+        $query = $this->db->query("SELECT BNIDBUID, BNDESB1 FROM t0021 WHERE BNIDBUID = '$bndesb1'");
+
+        return $query->row();
+    }
+
+    public function cek_user($bndesb1) {
+        $query = $this->db->query("SELECT SCIDBUID FROM t9801 WHERE SCIDBUID = '$bndesb1'");
+        return $query;
+
+        if($query->num_rows() > 0)
+            return 1;
+        else
+            return 0;
+    }
+
+    public function get_sequence($bndesb1) {
+        $query = $this->db->query("SELECT * FROM t9801 WHERE SCIDBUID = '$bndesb1' ORDER BY SCSEQ DESC LIMIT 1");
+
+        return $query->row();
+    }
+
+    public function Hapus_user($scidbuid, $scseq){
+        $this->db->query("UPDATE t9801 SET SCDVS = 'V' WHERE SCIDBUID = '$scidbuid' AND SCSEQ = '$scseq'");
+    }
+
+}
+
+/* End of file UserLogin_model.php */
+
+?>
--- application/views/UserLogin/index.php
+++ application/views/UserLogin/index.php
@@ -8,8 +8,8 @@
                         <center><h4 class="card-title">USER LOGIN</h4></center>
                     </div>
                     <div class="card-body">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fa fa-plus-square"></i>&nbsp;&nbsp;&nbsp;Tambah Data</button>
-                        <button type="button" class="btn btn-primary" style="float:right;" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Detail Pencarian Data &nbsp;&nbsp;<i class="fa fa-angle-double-down"></i></button>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg"><i class="fa fa-user-plus"></i>&nbsp;&nbsp;&nbsp;Tambah User</button>
+                        <!-- <button type="button" class="btn btn-primary" style="float:right;" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Detail Pencarian Data &nbsp;&nbsp;<i class="fa fa-angle-double-down"></i></button>
                         <div class="collapse" id="collapseExample">
                             <div class="card card-body">
                                 <div class="basic-form">
@@ -59,36 +59,42 @@
                                     </form>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <div class="table-responsive" style="margin-top:20px;">
                             <table id="example" class="display" style="min-width: 845px;color:#4b4b4b;">
                                 <thead>
                                     <tr>
                                         <th>No</th>
-                                        <th>User</th>
-                                        <th>Nomor Induk</th>
-                                        <th>Unit Kerja/OPD</th>
+                                        <th>Nama User</th>
+                                        <th>Username</th>
+                                        <th>Password</th>
                                         <th>Hak Akses</th>
                                         <th>Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>3</td>
-                                        <td>3</td>
-                                        <td>3</td>
-                                        <td>3</td>
-                                        <td>3</td>
-                                    </tr>
+                                    <?php $no=1; foreach($getAll as $gt):?>
+                                        <tr>
+                                        <td><?=$no++;?></td>
+                                        <td><?=$gt["BNDESB1"];?></td>
+                                        <td><?=$gt["SCUSI"];?></td>
+                                        <td><?=$gt["SCUSC"];?></td>
+                                        <td><?=$gt["SCUSG"];?></td>
+                                        <td>
+                                            <a data-toggle="modal" href="#basicModal<?=$gt["SCIDBUID"];?><?=$gt["SCSEQ"];?>" title="Edit Data" style="color:#2b2a28;"><span class="badge badge-success"><i class="fa fa-edit"></i>&nbsp;&nbsp;Edit</span></a>
+                                            <a data-toggle="modal" href="#basicModal2<?=$gt["SCIDBUID"];?><?=$gt["SCSEQ"];?>" title="Hapus Data" style="color:#2b2a28;"><span class="badge badge-danger"><i class="fa fa-trash"></i>&nbsp;&nbsp;Hapus</span></a>
+                                        </tr>
+                                    <?php endforeach;?>
                                 </tbody>
                             </table>
                         </div>
+                        
+                        <!-- MODAL TAMBAH USER START -->
                         <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
                             <div class="modal-dialog modal-lg">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title">Tambah User Login</h5>
+                                        <h5 class="modal-title">Tambah User</h5>
                                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                                     </div>
                                     <div class="modal-body">
@@ -98,39 +104,33 @@
                                                     <?php echo validation_errors(); ?>
                                                 </div>
                                             <?php endif; ?>
-                                            <form action="#" method="post">
+                                            <form action="<?=base_url()?>UserLogin/Tambah_user" method="post">
+                                                <input type="hidden" class="form-control" name="SCCOID" value="<?=$kode_kab->CNCOID;?>">
                                                 <div class="form-group col-md-12">
-                                                    <label>User</label>
-                                                    <input type="text" class="form-control" name="" autocomplete="off">
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <label>Email</label>
-                                                    <input type="email" class="form-control" name="" autocomplete="off">
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <label>Password</label>
-                                                    <input type="text" class="form-control" name="" autocomplete="off">
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <label>Ketik Ulang Password</label>
-                                                    <input type="text" class="form-control" name="" autocomplete="off">
-                                                </div>
-                                                <div class="form-group col-md-12">
-                                                    <label>Unit Kerja/OPD</label>
-                                                    <select id="inputState" class="form-control">
-                                                        <option selected>Choose...</option>
-                                                        <option>Option 1</option>
-                                                        <option>Option 2</option>
-                                                        <option>Option 3</option>
+                                                    <label style="color:#313236"><b>Unit Kerja/OPD</b></label>
+                                                    <select class="form-control" name="SCIDBUID" required>
+                                                        <option value="" selected="true" disabled="disabled">- Pilih OPD -</option>
+                                                        <?php foreach($opd as $opd) : ?>
+                                                            <option value="<?=$opd["BNIDBUID"]?>"><?=$opd["BNDESB1"]?></option>
+                                                        <?php endforeach;?>
                                                     </select>
                                                 </div>
                                                 <div class="form-group col-md-12">
-                                                    <label>Hak Akses</label>
-                                                    <input type="text" class="form-control" name="" autocomplete="off">
+                                                    <label style="color:#313236"><b>Username</b></label>
+                                                    <input type="text" class="form-control" name="SCUSI" autocomplete="off">
                                                 </div>
                                                 <div class="form-group col-md-12">
-                                                    <label>Nomor Induk</label>
-                                                    <input type="text" class="form-control" name="" autocomplete="off">
+                                                    <label style="color:#313236"><b>Password</b></label>
+                                                    <input type="text" class="form-control" name="SCUSC" autocomplete="off">
+                                                </div>
+                                                <div class="form-group col-md-12">
+                                                    <label style="color:#313236"><b>Hak Akses</b></label>
+                                                    <select class="form-control" name="SCUSG" required>
+                                                        <option value="" selected="true" disabled="disabled">- Pilih Jenis User -</option>
+                                                        <?php foreach($hak_akses as $ha) : ?>
+                                                            <option value="<?=$ha["DTDESC1"]?>"><?=$ha["DTDESC1"]?></option>
+                                                        <?php endforeach;?>
+                                                    </select>
                                                 </div>
                                             </div>
                                             <br>
@@ -143,6 +143,116 @@
                                 </div>
                             </div>
                         </div>
+                        <!-- MODAL TAMBAH USER END -->
+
+                        <!-- MODAL EDIT USER START -->
+                        <?php foreach($getAll as $gt):?>
+                            <div class="modal fade" id="basicModal<?=$gt["SCIDBUID"];?><?=$gt["SCSEQ"];?>">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Edit User</h5>
+                                            <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="basic-form">
+                                                <?php if (validation_errors()): ?>
+                                                    <div class="alert alert-danger" role="alert">
+                                                        <?php echo validation_errors(); ?>
+                                                    </div>
+                                                <?php endif; ?>
+                                                <form action="<?=base_url('UserLogin/Edit_user/'.$gt['SCIDBUID'].'/'.$gt["SCSEQ"])?>" method="post">
+                                                    <input type="hidden" class="form-control" name="SCCOID" value="<?=$gt["SCCOID"];?>">
+                                                    <input type="hidden" class="form-control" name="SCIDBUID" value="<?=$gt["SCIDBUID"];?>">
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-12">
+                                                            <label style="color:#313236"><b>Unit Kerja/OPD</b></label>
+                                                            <input type="text" class="form-control" name="SCBUID" autocomplete="off" value="<?=$gt["BNDESB1"];?>" readonly>
+                                                            <!-- <select class="form-control" name="SCIDBUID">
+                                                                <?php foreach($unit_kerja as $o) :?>
+                                                                    <?php if($o["BNIDBUID"] == $gt["SCIDBUID"]) {?>
+                                                                        <option value="<?=$gt["SCIDBUID"]?>" selected><?=$gt["BNDESB1"];?></option>
+                                                                    <?php }
+                                                                    else {?>
+                                                                        <option value="<?=$o["BNIDBUID"]?>"><?=$o["BNDESB1"]?></option>
+                                                                    <?php }?>
+                                                                <?php endforeach;?>
+                                                            </select> -->
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-12">
+                                                            <label style="color:#313236"><b>Username</b></label>
+                                                            <input type="text" class="form-control" name="SCUSI" autocomplete="off" value="<?=$gt["SCUSI"];?>">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-12">
+                                                            <label style="color:#313236"><b>Password</b></label>
+                                                            <input type="text" class="form-control" name="SCUSC" autocomplete="off" value="<?=$gt["SCUSC"];?>">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-row">
+                                                        <div class="form-group col-md-12">
+                                                            <label style="color:#313236"><b>Hak Milik</b></label>
+                                                            <select class="form-control" name="SCUSG">
+                                                                <?php foreach($akses as $a) :?>
+                                                                    <?php if($a["DTDESC1"] == $gt["SCUSG"]) {?>
+                                                                        <option value="<?=$gt["SCUSG"]?>" selected><?=$gt["SCUSG"];?></option>
+                                                                    <?php }
+                                                                    else {?>
+                                                                        <option value="<?=$a["DTDESC1"]?>"><?=$a["DTDESC1"]?></option>
+                                                                    <?php }?>
+                                                                <?php endforeach;?>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <!-- <br> -->
+                                                    <div class="form-group row">
+                                                        <div class="col-md-12">
+                                                            <center>
+                                                                <button type="submit" name="submit" class="btn btn-primary"><i class="fa fa-save"></i>&nbsp;&nbsp;Simpan Data</button>
+                                                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                                                <button type="button" class="btn btn-primary" class="close" data-dismiss="modal"><i class="fa fa-times-circle"></i>&nbsp;&nbsp;Batal</button>
+                                                            </center>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach;?>
+                        <!-- MODAL EDIT END -->
+
+                        <!-- MODAL HAPUS USER START -->
+                        <?php foreach($getAll as $gt):?>
+                            <div class="modal fade" id="basicModal2<?=$gt["SCIDBUID"];?><?=$gt["SCSEQ"];?>">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Hapus User</h5>
+                                            <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="basic-form">
+                                                <form method="post" action="<?=base_url('UserLogin/Hapus_user/'.$gt['SCIDBUID'].'/'.$gt["SCSEQ"])?>">
+                                                    <p style="color:#2b2a28;">Apakah Anda yakin ingin menghapus User dari <b><?=$gt["BNDESB1"];?></b>?</p>
+                                                    <div class="modal-footer">
+                                                        <input type="hidden" name="SCIDBUID" value="<?=$gt["SCIDBUID"];?>">
+                                                        <input type="hidden" name="SCSEQ" value="<?=$gt["SCSEQ"];?>">
+                                                        <button type="submit" name="submit" class="btn btn-danger"><i class="fa fa-trash"></i> &nbsp;&nbsp;Hapus</button>
+                                                        <button type="button" class="btn btn-primary" class="close" data-dismiss="modal"><i class="fa fa-times-circle"></i> &nbsp;&nbsp;Batal</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach;?>
+                        <!-- MODAL HAPUS USER END -->
                     </div>
                 </div>
             </div>
